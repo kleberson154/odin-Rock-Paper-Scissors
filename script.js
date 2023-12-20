23d1fba1d31e2021aed270a8858ffc8430d646dd
Kleberson Andrade <79817657+kleberson154@users.noreply.github.com>
2023-12-20 15:44:30 -0300

    Revert "Styled"

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
-let playerPoint = 0
-let compPoint = 0
-let roundWinner = ''
+let playerScore = 0
+let compScore = 0
 
-//buttons
-const rockBtn = document.getElementById('rockBtn')
-const paperBtn = document.getElementById('paperBtn')
-const scissorBtn = document.getElementById('scissorBtn')
-rockBtn.addEventListener('click', () => handleClick('rock'))
-paperBtn.addEventListener('click', () => handleClick('paper'))
-scissorsBtn.addEventListener('click', () => handleClick('scissor'))
-
-function handleClick(playerSelection) {
-  const computerSelection = getRandomChoice()
+function computerPlay(compChoice) {
+  let choiceNum = Math.floor(Math.random() * 3)
+  if (choiceNum == 0) {
+    compChoice.Value = 'pedra'
+  } else if (choiceNum == 1) {
+    compChoice.Value = 'papel'
+  } else if (choiceNum == 2) {
+    compChoice.Value = 'tesoura'
+  }
+  console.log(choiceNum)
+  return choiceNum
 }
 
-//jogada do computador
-function getRandomChoice() {
-  let randomNumber = Math.floor(Math.random() * 3)
-  switch (randomNumber) {
-    case 0:
-      return 'rock'
-    case 1:
-      return 'paper'
-    case 2:
-      return 'scissor'
+function playRound(playerChoiceInt, compChoiceInt, compChoice, playerChoice) {
+  /* 0 == pedra
+     1 == papel
+     2 == tesoura
+   */
+
+  let win_array = [
+    [0, 2, 1],
+    [1, 0, 2],
+    [2, 1, 0]
+  ]
+  let result = win_array[playerChoiceInt][compChoiceInt]
+  if (result == 0) {
+    console.log(
+      `É um empate! Você escolheu ${playerChoice} e o computador escolheu ${compChoice.Value}`
+    )
+  } else if (result == 1) {
+    console.log(
+      `Você ganhou! Você escolheu ${playerChoice} e o computador escolheu ${compChoice.Value}`
+    )
+    playerScore++
+  } else if (result == 2) {
+    console.log(
+      `Você perdeu! Você escolheu ${playerChoice} e o computador escolheu ${compChoice.Value}`
+    )
+    compScore++
   }
 }
+
+function game() {
+  let compChoice = { Value: '' }
+  let compChoiceInt
+  let playerChoiceInt
+  let playerChoice
+
+  for (let i = 0; i < 5; i++) {
+    playerChoice = prompt('pedra, papel, ou tesoura?').toLowerCase()
+    if (playerChoice == 'pedra') {
+      playerChoiceInt = 0
+    } else if (playerChoice == 'papel') {
+      playerChoiceInt = 1
+    } else if (playerChoice == 'tesoura') {
+      playerChoiceInt = 2
+    }
+    compChoiceInt = computerPlay(compChoice)
+    playRound(playerChoiceInt, compChoiceInt, compChoice, playerChoice)
+  }
+  console.log(`Player ${playerScore} x Computer ${compScore}`)
+}
+
+game()
